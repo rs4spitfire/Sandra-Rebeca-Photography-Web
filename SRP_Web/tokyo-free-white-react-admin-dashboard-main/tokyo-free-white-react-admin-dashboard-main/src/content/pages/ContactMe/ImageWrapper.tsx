fca2a9090ec68c27b461b3926ee538756d7090a2
rs4spitfire_SRP-at-545477882052 <rs4spitfire@gmail.com>
2023-01-12 19:48:46 -0500

    Update latest changes

---
+++ SRP_Web/tokyo-free-white-react-admin-dashboard-main/tokyo-free-white-react-admin-dashboard-main/src/content/pages/ContactMe/ImageWrapper.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
+//import ImageListItemBar from '@mui/material/ImageListItemBar';
+/*This goes underneath ImageListItem -- 
+            <ImageListItemBar
+             sx={{fontFamily:'Helvetica'}}
+            title={item.title}
+            subtitle="Test1"/>*/
+
+export default function ImageWrapper() {
+  return (
+    <Box sx={{ padding: "40px",width: "100%", alignItems:'center' }}>
+      <ImageList variant="standard" cols={1} gap={0}>
+        {itemData.map((item) => (
+          <ImageListItem key={item.img}>
+            <img
+              src={`${item.img}?w=248&fit=crop&auto=format`}
+              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.title}
+              loading="lazy"
+              title={item.title}
+            />
+             
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </Box>
+  );
+}
+
+const itemData = [
+    {
+        img: '/static/images/Photos/Sandra_Contact.jpg',
+        title: 'Sandra with Lens',
+      }  
+];
--- SRP_Web/tokyo-free-white-react-admin-dashboard-main/tokyo-free-white-react-admin-dashboard-main/src/content/pages/ContactMe/index.tsx
+++ SRP_Web/tokyo-free-white-react-admin-dashboard-main/tokyo-free-white-react-admin-dashboard-main/src/content/pages/ContactMe/index.tsx
@@ -1,8 +1,89 @@
-function ContactMePage(props){
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import ImageWrapper from './ImageWrapper';
+import ImageWrapperBottom from './ImageWrapperBottom';
+import {ThemeProvider,useTheme} from '@mui/material/styles'
+import{
+Container,
+Grid,
+Card,
+CardHeader,
+CardContent,
+Divider,
+Box,
+TextField,
+Stack,
+Button,
+Chip,
+Tooltip,
+Link,
+IconButton,
+FormGroup,
+FormControlLabel,
+Typography
+}from '@mui/material'
 
-    return(<>
-    <p>Contact Page Works</p>
-    </>);
+const Item = styled(Card)(({ theme }) => ({
+    ...theme.typography.body1,
+    borderRadius: '20px',
+    textAlign: 'left'
+  }));
+
+function ContactMePage(props){
+  const theme = useTheme();
+return(
+  <>
+      <Container maxWidth="lg">
+        <Grid container direction="row" justifyContent="start" spacing={1} sx={{padding:"20px"}}>
+           <Box component={Grid} item display={{xs: "none", sm: "block" }} sm={6} md={5} lg={4}> 
+             <Item>
+              <ImageWrapper></ImageWrapper>
+             </Item>
+            </Box>
+           
+            
+        <Grid item xs={8} md={4} lg={6} paddingBottom={5}>
+        <Card variant="outlined">
+          <CardHeader title="Contact Sandra Rebeca" />
+          <CardContent>
+          <Box component="form" autoComplete="off">
+            <Grid
+            container
+            direction="row"
+            justifyContent="start"
+            
+            spacing={1}>
+              <Grid item xs={12} lg={6}>
+                  <TextField label="Name" variant="outlined" sx={{pb:1}} />
+              </Grid>
+              <Grid item xs={12} lg={6}>
+                  <TextField label="Phone" variant="outlined" sx={{pb:1}} />
+              </Grid>
+              <Grid item xs={12} lg={6}>
+                  <TextField label="Email" variant="outlined" sx={{pb:1}} />
+              </Grid>
+              <Grid item xs={12} lg={6}>
+                  <TextField label="Home City" variant="outlined" sx={{pb:1}} />
+              </Grid>
+              <Grid item xs={12} lg={6}>
+                  <TextField sx={{pb:1}} InputLabelProps={{style: {fontSize: 12}}} id="standard-basic" label="One place you would like to visit" variant="outlined" />
+              </Grid>
+              <Grid item xs={12} lg={6}>
+                  <TextField sx={{pb:1}} InputLabelProps={{style: {fontSize: 12}}} id="standard-basic" label="Favorite Photography Location" variant="outlined" /> 
+              </Grid>
+            </Grid>
+          </Box>
+          </CardContent>
+            <Stack direction="row" spacing={2} justifyContent="flex-end" sx={{padding:1}}>
+              <Button variant="contained">Submit</Button>
+            </Stack>
+        </Card>
+        </Grid>
+        </Grid>
+        </Container>
+  
+  </>
+      );
 }
 
 export default ContactMePage;
