78c7eeca0d7c9fd69710c11de8e4ed81216143c8
rs4spitfire_SRP-at-545477882052 <rs4spitfire@gmail.com>
2023-02-12 12:13:15 -0500

    Add Pop Up Menu for Small Screen Size

--- SRP_Web/tokyo-free-white-react-admin-dashboard-main/tokyo-free-white-react-admin-dashboard-main/src/layouts/SidebarLayout/Header/Menu/index.tsx
+++ SRP_Web/tokyo-free-white-react-admin-dashboard-main/tokyo-free-white-react-admin-dashboard-main/src/layouts/SidebarLayout/Header/Menu/index.tsx
@@ -1,5 +1,6 @@
-import { Typography, Badge, useTheme } from '@mui/material';
+import { Typography, Badge, useTheme,Popover,Card,CardContent } from '@mui/material';
 import { useRef, useState,useEffect } from 'react';
+import {ClickAwayListener} from '@mui/base';
 
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
@@ -13,22 +14,46 @@ import IconButton from '@mui/material/IconButton';
 
 import MenuIcon from '@mui/icons-material/Menu';
 
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  console.log("Window Size: "+window.innerWidth);
+  return {
+    
+    width,
+    height
+  };
+}
+
 
 
 function HeaderTitle() {
   
-  
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  const divRef=useRef();
+  const [anchorEl,setAnchorEl]=useState();
+  const open=Boolean(anchorEl);
+  const id = open?"simple-popover":undefined;
+
+  function openPopUp(){
+    console.log("click");
+    console.log(divRef)
+    setTimeout(()=>setAnchorEl(divRef?.current),1)
+  };
+
+  function closePopUp(){
+    //console.log(divRef);
+    setAnchorEl(null);
+  }
 
   useEffect(() => {
-
     function handleResize() {
-      console.log('resized to: ', window.innerWidth, 'x', window.innerHeight);
-      windowSize=window.innerWidth;
+      setWindowDimensions(getWindowDimensions());
+      closePopUp();
+    }
 
-}
-
-    window.addEventListener('resize', handleResize)
-  })
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   const theme = useTheme();
 
@@ -42,25 +67,49 @@ function HeaderTitle() {
     />
   </Link>
       <Box sx={{ width: '100%' }}>
-     <div><Button disableRipple component={RouterLink} to="/pages/Portfolio">Portfolio</Button>
-        <Button disableRipple component={RouterLink} to="/">Journal</Button>
+    { window.innerWidth>=760 && <div><Button disableRipple component={RouterLink} to="/pages/Portfolio">Portfolio</Button>
+        <Button disableRipple  component={RouterLink} to="/">Journal</Button>
         <Button disableRipple component={RouterLink} to="/pages/AboutMe">About Me</Button>
-        <Button disableRipple component={RouterLink} to="/Pages/ContactMe">Contact</Button></div>
+        <Button disableRipple  component={RouterLink} to="/Pages/ContactMe">Contact</Button></div>}
+   
         
     </Box>
-    <IconButton
+    <Popover
+        id={id}
+        open={open}
+        anchorEl={divRef.current}
+        anchorOrigin={{
+          vertical:'top',
+          horizontal:'center'
+        }}
+      ><ClickAwayListener onClickAway={closePopUp}><Card variant="outlined" onBlur={closePopUp}>
+        <CardContent>
+        <div><Button disableRipple onClick={closePopUp} component={RouterLink} to="/pages/Portfolio">Portfolio</Button></div>
+        <div><Button disableRipple onClick={closePopUp} component={RouterLink} to="/">Journal</Button></div>
+        <div><Button disableRipple onClick={closePopUp} component={RouterLink} to="/pages/AboutMe">About Me</Button></div>
+        <Button disableRipple onClick={closePopUp} component={RouterLink} to="/Pages/ContactMe">Contact</Button>
+        </CardContent>        
+        </Card>  
+      </ClickAwayListener>    
+    </Popover>
+    {window.innerWidth<760 && <IconButton
+      //fontSize={`${window.innerWidth<760 ? "large": "medium"}`}
+      
+      //onClick={openPopUp}
+      //onBlur={closePopUp}
+      ref={divRef}
         aria-label="more"
         id="hamburger-button"
         aria-controls={open ? 'long-menu' : undefined}
         aria-expanded={open ? 'true' : undefined}
         aria-haspopup="true"
         sx={{color: theme.colors.secondary.dark}}
-        //onClick={handleClick}
+        onClick={openPopUp}
       >
-        <MenuIcon/>
-      </IconButton>
-    <Link color={theme.colors.secondary.dark} underline="none" href="https://www.instagram.com/sandrar_photography/?hl=en" target="_blank">
-    <InstagramIcon></InstagramIcon>
+        <MenuIcon fontSize="large"></MenuIcon>
+      </IconButton>}
+      <Link color={theme.colors.secondary.dark} underline="none" href="https://www.instagram.com/sandrar_photography/?hl=en" target="_blank">
+    <InstagramIcon fontSize={`${window.innerWidth<760 ? "large": "small"}`} ></InstagramIcon>
     </Link>
     </>
   );
